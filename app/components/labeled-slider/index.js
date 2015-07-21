d4274e1c71ad422274391b0b441192a301ddebf2
Pascal Hertleif <killercup@gmail.com>
2015-07-21 14:57:04 +0200

    feat(example): Add BMI Cycle Example Code

---
+++ app/components/labeled-slider/index.js
@@ -0,0 +1,32 @@
+import {Rx} from '@cycle/core';
+import {hJSX} from '@cycle/dom';
+
+export default function labeledSlider(responses) {
+  let initialValue$ = responses.props.get('initial').first();
+  let newValue$ = responses.DOM.get('.slider', 'input')
+    .map(ev => ev.target.value);
+
+  let value$ = initialValue$.concat(newValue$);
+  let props$ = responses.props.get('*');
+
+  let vtree$ = Rx.Observable
+    .combineLatest(props$, value$, (props, value) =>
+      <div className="labeled-slider">
+        <div className="label">
+          {props.children.concat(value + props.unit)}
+        </div>
+        <input className="slider" type="range"
+          min={props.min}
+          max={props.max}
+          value={value}
+          />
+      </div>
+    );
+
+  return {
+    DOM: vtree$,
+    events: {
+      newValue: newValue$,
+    },
+  };
+}
--- app/index.js
+++ app/index.js
@@ -1,15 +1,55 @@
-import Cycle from '@cycle/core';
-import {h, makeDOMDriver} from '@cycle/dom';
+import Cycle, {Rx} from '@cycle/core';
+import {hJSX, makeDOMDriver} from '@cycle/dom';
 
-function main() {
+function calculateBMI(weight: number, height: number) {
+  const heightMeters = height * 0.01;
+  return Math.round(weight / (heightMeters * heightMeters));
+}
+
+function intent(DOM) {
   return {
-    DOM: Cycle.Rx.Observable.interval(1000)
-      .map(i => <h1>{`${i} seconds elapsed`}</h1>)
+    changeWeight: DOM.get('#weight', 'newValue').map(ev => ev.detail),
+    changeHeight: DOM.get('#height', 'newValue').map(ev => ev.detail),
   };
 }
 
-let drivers = {
-  DOM: makeDOMDriver('#app')
-};
+function model(actions) {
+  return Rx.Observable
+    .combineLatest(
+      actions.changeWeight.startWith(70),
+      actions.changeHeight.startWith(170),
+      (weight, height) => ({
+        weight,
+        height,
+        bmi: calculateBMI(weight, height),
+      })
+    );
+}
 
-Cycle.run(main, drivers);
+function view(state) {
+  return state.map(({weight, height, bmi}) =>
+    <div>
+      <labeled-slider id="weight" key={1}
+        unit="kg" min={40} initial={weight} max={140}>
+        <h2>Weight</h2>
+      </labeled-slider>
+      <labeled-slider id="height" key={2}
+        unit="cm" min={140} initial={height} max={210}>
+        <h4>Height</h4>
+      </labeled-slider>
+      <h2>{`BMI is ${bmi}`}</h2>
+    </div>
+  );
+}
+
+function main({DOM}) {
+  return {
+    DOM: view(model(intent(DOM))),
+  };
+}
+
+Cycle.run(main, {
+  DOM: makeDOMDriver('#app', {
+    'labeled-slider': require('./components/labeled-slider'),
+  }),
+});
