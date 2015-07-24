33e9c099ba830ad78c91416df2dc1b72312544cc
Pascal Hertleif <killercup@gmail.com>
2015-07-24 21:57:18 +0200

    fix(example): Parse Slider Value to Integer
    Fixes #2

--- app/components/labeled-slider/index.js
+++ app/components/labeled-slider/index.js
@@ -2,14 +2,14 @@ import {Rx} from '@cycle/core';
 import {hJSX} from '@cycle/dom';
 
 export default function labeledSlider(responses) {
-  let initialValue$ = responses.props.get('initial').first();
-  let newValue$ = responses.DOM.get('.slider', 'input')
-    .map((ev) => ev.target.value);
+  const initialValue$ = responses.props.get('initial').first();
+  const newValue$ = responses.DOM.get('.slider', 'input')
+    .map((ev) => +ev.target.value);
 
-  let value$ = initialValue$.concat(newValue$);
-  let props$ = responses.props.get('*');
+  const value$ = initialValue$.concat(newValue$);
+  const props$ = responses.props.get('*');
 
-  let vtree$ = Rx.Observable
+  const vtree$ = Rx.Observable
     .combineLatest(props$, value$, (props, value) =>
       <div className="labeled-slider">
         <div className="label">
